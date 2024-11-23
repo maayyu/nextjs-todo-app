6e8154cd4f120084d055b0dd847464b1472a7948
maayuu <reeeeedrabbit@yahoo.co.jp>
2024-11-23 23:23:31 +0900

    一覧ページ編集

--- app/page.tsx
+++ app/page.tsx
@@ -1,18 +1,30 @@
 /*TODO一覧ページ*/
+"use client";
+
+import { useEffect, useState } from "react";
 import Link from "next/link";
+import { getTodos } from "../lib/supabaseClient";
 
 export default function Home() {
+  const [todos, setTodos] = useState<any[]>([]);
+
+  useEffect(() => {
+    const fetchTodos = async () => {
+      const data = await getTodos();
+      setTodos(data);
+    };
+    fetchTodos();
+  }, []);
+
   return (
     <div>
       <h1>TODO一覧</h1>
-      {/* TODOリストをここに表示 */}
       <ul>
-        <li>
-          サンプルTODO 1 - <Link href="/details/1">詳細を見る</Link>
-        </li>
-        <li>
-          サンプルTODO 2 - <Link href="/details/2">詳細を見る</Link>
-        </li>
+        {todos.map((todo) => (
+          <li key={todo.id}>
+            {todo.title} - <Link href={`/details/${todo.id}`}>詳細を見る</Link>
+          </li>
+        ))}
       </ul>
       <Link href="/create">新しいTODOを作成</Link>
     </div>
